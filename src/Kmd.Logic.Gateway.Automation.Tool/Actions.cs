78569cc0c23164c6a2de47d157414e914870cdec
Jacek Stec (JTK) <JTK@kmd.dk>
2020-10-01 11:33:10 +0200

    added tool project and fixed directory.build.props

---
+++ src/Kmd.Logic.Gateway.Automation.Tool/Actions.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Serilog;
+
+namespace Kmd.Logic.Gateway.Automation.Tool
+{
+    internal class Actions
+    {
+        public Task<int> Publish()
+        {
+            Log.Information("Published");
+            return Task.FromResult(0);
+        }
+    }
+}
