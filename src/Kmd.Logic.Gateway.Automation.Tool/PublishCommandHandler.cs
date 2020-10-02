b7cb731b057346abeae769a9a14634088d3c9869
Jacek Stec (JTK) <JTK@kmd.dk>
2020-10-02 09:03:51 +0200

    temporary suppress

--- src/Kmd.Logic.Gateway.Automation.Tool/PublishCommandHandler.cs
+++ src/Kmd.Logic.Gateway.Automation.Tool/PublishCommandHandler.cs
@@ -3,10 +3,11 @@ using Serilog;
 
 namespace Kmd.Logic.Gateway.Automation.Tool
 {
-
     internal class PublishCommandHandler
     {
+#pragma warning disable CA1801 // Remove unused parameter
         public Task<int> Handle(PublishCommand cmd)
+#pragma warning restore CA1801 // Remove unused parameter
         {
             Log.Information("Published");
             return Task.FromResult(0);
