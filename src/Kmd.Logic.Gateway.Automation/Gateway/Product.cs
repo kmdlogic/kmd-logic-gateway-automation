c9c6133034d083c6fdca2a1f530f8c26492d5da3
Basavaraj Araballi <rajuaraballi@outlook.com>
2020-10-19 12:18:19 +0530

    removed warning

--- src/Kmd.Logic.Gateway.Automation/Gateway/Product.cs
+++ src/Kmd.Logic.Gateway.Automation/Gateway/Product.cs
@@ -30,7 +30,9 @@ namespace Kmd.Logic.Gateway.Automation.Gateway
 
         public string OpenidConfigIssuer { get; set; }
 
+#pragma warning disable CA1056 // URI-like properties should not be strings
         public string OpenidConfigCustomUrl { get; set; }
+#pragma warning restore CA1056 // URI-like properties should not be strings
 
         public string ApplicationId { get; set; }
     }
