861898056bfff2af428fb4c11fb78533c23c1c1d
Jacek Stec (JTK) <JTK@kmd.dk>
2020-10-30 09:23:10 +0100

    removed underscores

--- src/Kmd.Logic.Gateway.Automation/GatewayClientFactory.cs
+++ src/Kmd.Logic.Gateway.Automation/GatewayClientFactory.cs
@@ -6,23 +6,23 @@ namespace Kmd.Logic.Gateway.Automation
 {
     internal class GatewayClientFactory
     {
-        private readonly LogicTokenProviderFactory _logicTokenProviderFactory;
-        private readonly HttpClient _httpClient;
-        private readonly GatewayOptions _gatewayOptions;
+        private readonly LogicTokenProviderFactory logicTokenProviderFactory;
+        private readonly HttpClient httpClient;
+        private readonly GatewayOptions gatewayOptions;
 
         public GatewayClientFactory(LogicTokenProviderFactory logicTokenProviderFactory, HttpClient httpClient, GatewayOptions gatewayOptions)
         {
-            this._logicTokenProviderFactory = logicTokenProviderFactory;
-            this._httpClient = httpClient;
-            this._gatewayOptions = gatewayOptions;
+            this.logicTokenProviderFactory = logicTokenProviderFactory;
+            this.httpClient = httpClient;
+            this.gatewayOptions = gatewayOptions;
         }
 
         public IGatewayClient CreateClient()
         {
-            var tokenProvider = this._logicTokenProviderFactory.GetProvider(this._httpClient);
+            var tokenProvider = this.logicTokenProviderFactory.GetProvider(this.httpClient);
             var client = new GatewayClient(new TokenCredentials(tokenProvider))
             {
-                BaseUri = this._gatewayOptions.GatewayServiceUri,
+                BaseUri = this.gatewayOptions.GatewayServiceUri,
             };
             return client;
         }
