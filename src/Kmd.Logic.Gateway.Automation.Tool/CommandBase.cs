1ef256362a964aa8220ca0a66fc0512929a7e058
Jacek Stec (JTK) <JTK@kmd.dk>
2020-11-03 09:24:41 +0100

    removed short names for client credentials parameters

--- src/Kmd.Logic.Gateway.Automation.Tool/CommandBase.cs
+++ src/Kmd.Logic.Gateway.Automation.Tool/CommandBase.cs
@@ -8,13 +8,13 @@ namespace Kmd.Logic.Gateway.Automation.Tool
         [Option('f', "folderPath", Required = true, HelpText = "Path of the root folder with 'publish.yml' file.")]
         public string FolderPath { get; set; }
 
-        [Option("authorizationScope", HelpText = "Authorization scope in Logic Subscription Client Credentials.")]
+        [Option("scope", HelpText = "Authorization scope in Logic Subscription Client Credentials.")]
         public string AuthorizationScope { get; set; } = "https://logicidentityprod.onmicrosoft.com/bb159109-0ccd-4b08-8d0d-80370cedda84/.default";
 
         [Option("clientId", Required = true, HelpText = "Client ID in Logic Subscription Client Credentials.")]
         public string ClientId { get; set; }
 
-        [Option("clientSecret", Required = true, HelpText = "Client secret in Logic Subscription Client Credentials.")]
+        [Option("secret", Required = true, HelpText = "Client secret in Logic Subscription Client Credentials.")]
         public string ClientSecret { get; set; }
 
         [Option('p', "providerId", Required = true, HelpText = "Provider ID in Logic.")]
@@ -23,7 +23,7 @@ namespace Kmd.Logic.Gateway.Automation.Tool
         [Option('s', "subscriptionId", Required = true, HelpText = "Subscription ID in Logic.")]
         public Guid SubscriptionId { get; set; }
 
-        [Option("verbose", HelpText = "Logs everything")]
+        [Option('v', "verbose", HelpText = "Logs everything.")]
         public bool Verbose { get; set; }
     }
 }
